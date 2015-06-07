2d4c83895bb01e0cd9de65119f3c13de646d53d0
parasmanibatra <parasmanibatra@yahoo.co.in>
2015-06-08 00:16:11 +0530

    Added Service to communicate with AppRemina

--- Solution/AppRemina/AppRemina.cs
+++ Solution/AppRemina/AppRemina.cs
@@ -2,10 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using AppReminaInterfaces;
 
 namespace AppRemina
 {
-    public class AppRemina
+    public class AppReminaService : IAuthenticationService
     {
+        public bool Ping()
+        {
+            return true;
+        }
     }
 }
--- Solution/AppReminaLauncher/AppReminaLauncher.cs
+++ Solution/AppReminaLauncher/AppReminaLauncher.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.ServiceModel;
+using System.ServiceModel.Description;
 
 namespace AppReminaLauncher
 {
@@ -9,10 +11,23 @@ namespace AppReminaLauncher
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Here the app server will be launched.");
-            Console.WriteLine("aa");
-            Console.WriteLine("bb");
-            Console.ReadLine();
+            using (ServiceHost host = new ServiceHost(typeof(AppRemina.AppReminaService), 
+                new Uri("http://localhost:8083/AppReminaService")))
+            {
+                host.AddServiceEndpoint("AppReminaInterfaces.IAuthenticationService", new BasicHttpBinding(),
+                    "AppReminaService");
+                ServiceMetadataBehavior metaDataBehaviour = host.Description.Behaviors.Find<ServiceMetadataBehavior>();
+                if (metaDataBehaviour == null)
+                {
+                    metaDataBehaviour = new ServiceMetadataBehavior();
+                    metaDataBehaviour.HttpGetEnabled = true;
+                }
+                host.Description.Behaviors.Add(metaDataBehaviour);
+                host.Open();
+
+                Console.WriteLine("Press any key to terminate host");
+                Console.Read();
+            }
         }
     }
 }
