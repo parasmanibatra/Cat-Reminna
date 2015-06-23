161689d6653f7b342b8c1d785bf26aa920fbc93b
devum99 <devum99@gmail.com>
2015-06-24 00:02:09 +0530

    Modified for : Display message after authentication

--- Solution/WebRemina/JSFiles/appController.js
+++ Solution/WebRemina/JSFiles/appController.js
@@ -8,10 +8,12 @@
             url: serviceurl + '/' + uname + '/' + pwd
         }).success(function (data) {
             if (data == 'true') {
-                alert("Hi Paras!");
+                //  alert("Hi Paras!");
+                $scope.User = 'Hi User!!Site is under construction';
             }
             else {
                 alert("Invalid loginname or password!!");
+                $scope.User = 'Please register before login'
             }
         }).error(function () {
             alert("Ooops..Something went wrong!!");
